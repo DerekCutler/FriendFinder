09b6a7487e254f0c428826332f11103ad691355b
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2019-02-12 15:54:05 -0700

    Twerking?

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # FriendFinder
-This full-stack site will take in results from your users' surveys, then compare their answers with those from other users
---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+
+// ==========================================
+//This file stores the object that will keep all of the friends, similar to a sql database
+// ==========================================
+
+var friendObject = [
+    {name: "Jerry Seinfeld",
+    photo: "https://pbs.twimg.com/profile_images/1004767316553359360/XDLckmK0_400x400.jpg",
+    scores: [
+        4,
+        3,
+        2,
+        1,
+        5,
+        2,
+        4,
+        4,
+        3,
+        1
+    ]
+    },
+    {name: "George Costanza",
+    photo: "https://i.etsystatic.com/5851351/r/il/9d0f6a/380160499/il_570xN.380160499_gd9j.jpg",
+    scores: [
+        1,
+        2,
+        3,
+        2,
+        1,
+        1,
+        2,
+        2,
+        3,
+        1
+    ]
+    },
+    {name: "Elaine Benes",
+    photo: "https://pbs.twimg.com/profile_images/765352440652632064/q8cIAf92_400x400.jpg",
+    scores: [
+        5,
+        4,
+        4,
+        4,
+        5,
+        4,
+        4,
+        5,
+        3,
+        4
+    ]
+    },
+    {name: "Cosmo Kramer",
+    photo: "https://imgc.allpostersimages.com/img/print/u-g-F57Z7X0.jpg?w=550&h=550&p=0",
+    scores: [
+        2,
+        2,
+        1,
+        1,
+        1,
+        2,
+        1,
+        1,
+        1,
+        4
+    ]
+    }
+]
+
+// ==========================================
+//This exports the object so it can be used in other files
+// ==========================================
+module.exports = friendObject
